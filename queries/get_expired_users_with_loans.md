a85e288143cc97c2313585b3e73e6339a979d053
Maccabee Levine <msl321@lehigh.edu>
2025-03-28 16:48:35 -0400

    Write first draft of get_expired_users_with_loans

---
+++ queries/get_expired_users_with_loans.md
@@ -0,0 +1,21 @@
+# List expired users with loans
+
+## Purpose
+This report shows a list of expired users who still have outstanding loans.  So Mark can yell at them.
+
+## Parameters
+
+## Output table
+
+| Attribute | Type | Description | Sample output |
+| --- | --- | --- | --- |
+| barcode | TEXT | The barcode assigned to the item | 39151010192645 |
+| call_number | TEXT | Effective call number of the item | Baldwin, James, 1924-1987 |
+| title | TEXT | Book title | The price of the ticket : collected nonfiction: 1948-1985 / James Baldwin. |
+| item_status | TEXT | The status of the item | Available |
+| status_date | DATE | Date the item status changed | 2 |
+| loan_due_date | DATE | Date the loan was due | 2 |
+| patron_group | TEXT | The name of the patron group(s) | staff, faculty |
+| instance_hrid | INTEGER | FOLIO instance hrid | 00011881114 |
+
+There are more things to fix up here!!!
---
+++ queries/get_expired_users_with_loans.sql
@@ -0,0 +1,51 @@
+--metadb:function get_expired_users_with_loans
+
+DROP FUNCTION IF EXISTS get_expired_users_with_loans;
+
+CREATE FUNCTION get_expired_users_with_loans(
+)
+RETURNS TABLE (
+    item_barcode TEXT,
+    call_number TEXT,
+    title TEXT,
+    status_name TEXT,
+    status_date DATE,
+    loan_due_date DATE,
+    loan_policy_name TEXT,
+    patron_group_name TEXT,
+    user_barcode TEXT,
+    username TEXT,
+    last_name TEXT,
+    expire_date DATE
+)
+AS
+$$
+SELECT 
+    ie.barcode :: TEXT as item_barcode, 
+    ie.effective_call_number, 
+    ihi.title, ie.status_name, 
+    cast(ie.status_date as DATE) as status_date, 
+    cast(li.loan_due_date as DATE) as loan_due_date, 
+    li.loan_policy_name, 
+    li.patron_group_name, 
+    ug2.barcode AS user_barcode, 
+    ug2.username AS username, 
+    ug2.user_last_name AS last_name, 
+    cast(ug2.expiration_date as DATE) as expire_date
+FROM folio_derived.loans_items li
+JOIN folio_derived.item_ext ie 
+    on li.item_id = ie.item_id
+JOIN folio_derived.item_ext ie 
+    on li.item_id = ie.item_id
+JOIN folio_derived.items_holdings_instances ihi 
+    on li.item_id = ihi.item_id
+JOIN folio_derived.item_ext ie 
+    on li.item_id = ie.item_id
+JOIN folio_derived.users_groups ug2 
+    on li.user_id = ug2.user_id
+WHERE li.loan_status = 'Open' 
+    and ug2.expiration_date is not null 
+    and ie.discovery_suppress = 'False'
+ORDER BY effective_call_number desc;
+$$
+LANGUAGE SQL;
