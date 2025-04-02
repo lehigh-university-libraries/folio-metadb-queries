ca66f128b0ad8d2ae3d6d613419eb4533dec4bdd
Mark Canney <113544083+mac609@users.noreply.github.com>
2025-04-02 14:38:19 -0400

    Update get_expired_users_with_loans.sql
    replace username with user_email

--- queries/get_expired_users_with_loans.sql
+++ queries/get_expired_users_with_loans.sql
@@ -5,18 +5,18 @@ DROP FUNCTION IF EXISTS get_expired_users_with_loans;
 CREATE FUNCTION get_expired_users_with_loans(
 )
 RETURNS TABLE (
-    expire_date DATE,
-	last_name TEXT,
-	user_barcode TEXT,
-    username TEXT,
-	patron_group_name TEXT,
-	loan_due_date DATE,
-	item_barcode TEXT,
-    call_number TEXT,
-    title TEXT,
-	loan_policy_name TEXT,
-	status_date DATE,
-    status_name TEXT
+expire_date DATE,
+last_name TEXT,
+user_barcode TEXT,
+user_email TEXT,
+patron_group_name TEXT,
+loan_due_date DATE,
+item_barcode TEXT,
+call_number TEXT,
+title TEXT,
+loan_policy_name TEXT,
+status_date DATE,
+status_name TEXT
 )
 AS
 $$
@@ -24,7 +24,7 @@ SELECT
     cast(ug2.expiration_date as DATE) as expire_date,
     ug2.user_last_name AS last_name,
     ug2.barcode AS user_barcode, 
-    ug2.username AS username,
+    ug2.user_email AS user_email,
     li.patron_group_name,
     cast(li.loan_due_date as DATE) as loan_due_date, 
     ie.barcode :: TEXT as item_barcode, 
