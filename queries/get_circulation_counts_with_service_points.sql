4e78542a374ea95b8a63c18192659db789dc4d3b
Mark Canney <113544083+mac609@users.noreply.github.com>
2025-06-19 11:32:30 -0400

    Update get_circulation_counts_with_service_points.sql
    add DMS

--- queries/get_circulation_counts_with_service_points.sql
+++ queries/get_circulation_counts_with_service_points.sql
@@ -23,7 +23,6 @@ WITH checkout_actions AS (
     WHERE 
         (start_date IS NULL OR loan_date >= start_date)
         AND (end_date IS NULL OR loan_date < end_date)
-        AND checkout_service_point_name != 'Digital Media Studio'
     GROUP BY service_point_name, month_start
 ),
 simple_return_dates AS (
