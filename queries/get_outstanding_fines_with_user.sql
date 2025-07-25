16a1e5423f4b7cf1cacba37a7d41dedf65bffb2d
Mark Canney <113544083+mac609@users.noreply.github.com>
2025-07-25 11:46:15 -0400

    Update get_outstanding_fines_with_user.sql
    remove due_date

--- queries/get_outstanding_fines_with_user.sql
+++ queries/get_outstanding_fines_with_user.sql
@@ -11,8 +11,7 @@ RETURNS TABLE
     user_barcode TEXT,
     username TEXT,
     user_last_name TEXT,
-    item_title TEXT
-    item_due_date DATE,
+    item_title TEXT,
     item_call_number TEXT,
     item_barcode TEXT
 ) 
