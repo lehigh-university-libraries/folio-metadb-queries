1192db2e9edb9aaea6fa011024366ff56740c378
Mark Canney <113544083+mac609@users.noreply.github.com>
2025-10-16 10:16:49 -0400

    Create function to get major inventoried items
    This function retrieves items with a Major Inventory Condition Note, filtered by specified library locations.

---
+++ queries/get_items_inventoried_condition_note_major.sql
@@ -0,0 +1,30 @@
+--metadb:function get_items_inventoried_condition_note_major
+-- This function retrieves all items with an Inventory Condition Note that is Major, filtered by location.
+CREATE FUNCTION get_items_inventoried_condition_note_major(location_filter TEXT[] DEFAULT ARRAY['Linderman Library', 'Fairchild Martindale Library', 'Library Materials Center'])
+RETURNS TABLE
+(
+    library_name TEXT,
+    item_barcode TEXT,
+    item_call_number TEXT,
+    item_title TEXT,
+    item_updated_date DATE
+) 
+AS
+$$
+SELECT
+    ll.library_name,
+    ihi.barcode AS item_barcode,
+    ie.effective_call_number AS item_call_number,
+    ihi.title AS item_title,
+    CAST(ie.updated_date AS DATE) AS item_updated_date
+FROM
+    folio_derived.items_holdings_instances ihi
+    JOIN folio_derived.item_notes in2 ON in2.item_id = ihi.item_id
+    JOIN folio_derived.item_ext ie ON ie.item_id = ihi.item_id
+    JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
+WHERE
+    in2.note_type_name = 'Inventoried Condition'
+    AND in2.note LIKE 'Major%'
+    AND ll.library_name = ANY(location_filter)
+$$
+LANGUAGE SQL;
