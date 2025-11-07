7df46d1c9b74100dc142767d78422a12dce497fa
Mark Canney <113544083+mac609@users.noreply.github.com>
2025-11-07 12:31:56 -0500

    Refactor SQL query for graduating patrons with loans

--- queries/get_graduating_patrons_with_loans.sql
+++ queries/get_graduating_patrons_with_loans.sql
@@ -18,36 +18,36 @@ RETURNS TABLE
 ) 
 AS
 $$
-select cast (fug.updated_date as DATE) as graduating_note_date, 
-fug.barcode as patron_barcode, 
-fug.user_last_name as patron_last_name, 
+select cast (fug.updated_date as DATE) as graduating_note_date,
+fug.barcode as patron_barcode,
+fug.user_last_name as patron_last_name,
 fug.user_email AS patron_email,
 fug.group_name as patron_group,
-ie.barcode as item_barcode, 
-ie.effective_call_number as item_call_number, 
-ihi.title as item_title, 
-ie.status_name as loan_status,  
-cast(li.loan_due_date as DATE) as loan_due_date, 
+ie.barcode as item_barcode,
+ie.effective_call_number as item_call_number,
+ihi.title as item_title,
+ie.status_name as loan_status,
+cast(li.loan_due_date as DATE) as loan_due_date,
 li.loan_policy_name
 FROM folio_derived.loans_items li
-JOIN folio_derived.item_ext ie 
+JOIN folio_derived.item_ext ie
     on li.item_id = ie.item_id
-JOIN folio_derived.items_holdings_instances ihi 
+JOIN folio_derived.items_holdings_instances ihi
     on li.item_id = ihi.item_id
-JOIN folio_derived.users_groups fug 
+JOIN folio_derived.users_groups fug
     on li.user_id = fug.user_id
 join folio_notes.link fnl
-	on fnl.object_id = fug.user_id 
+	on fnl.object_id::uuid = fug.user_id 
 join folio_notes.note_link fnnl
 	on fnnl.link_id = fnl.id
 join folio_notes.note fnn
 	on fnn.id = fnnl.note_id
-join folio_notes.type__ t 
-	on t.id = fnn.type_id  
-where fug.active ='true' 
+join folio_notes.type t
+	on t.id = fnn.type_id
+where fug.active ='true'
 and fnl.object_type = 'user'
 and t.name = 'graduating'
-and li.loan_status = 'Open' 
+and li.loan_status = 'Open'
 and fnn.content = 'true'
 order by patron_last_name;
 $$
