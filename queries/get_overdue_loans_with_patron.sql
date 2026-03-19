a643908e1e990f0947c8412563a55d7a611ff2ec
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-03-19 12:14:38 -0400

    Refactor overdue loans query to use subquery for loans

--- queries/get_overdue_loans_with_patron.sql
+++ queries/get_overdue_loans_with_patron.sql
@@ -28,7 +28,15 @@ SELECT to_char(li.loan_due_date::TIMESTAMP, 'YYYY-MM-DD') AS loan_due_date,
         ug.user_email AS patron_email
 FROM folio_derived.items_holdings_instances ihi
         JOIN folio_derived.item_ext ie ON ie.item_id = ihi.item_id
-        JOIN folio_derived.loans_items li ON ihi.item_id = li.item_id
+        JOIN (
+            SELECT DISTINCT ON (item_id)
+                item_id, loan_due_date, item_status,
+                patron_group_name, user_id, loan_status
+            FROM folio_derived.loans_items
+            WHERE loan_status = 'Open'
+            AND loan_due_date < CURRENT_DATE
+            ORDER BY item_id, loan_due_date DESC
+        ) li ON ihi.item_id = li.item_id
         JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
         JOIN (
             SELECT DISTINCT ON (user_id)
@@ -36,9 +44,7 @@ FROM folio_derived.items_holdings_instances ihi
             FROM folio_derived.users_groups
             ORDER BY user_id
         ) ug ON li.user_id = ug.user_id
-WHERE li.loan_status = 'Open'
-        AND li.loan_due_date < CURRENT_DATE
-        AND li.patron_group_name NOT IN ('ill', 'palciuser', 'libraryuse')
+WHERE li.patron_group_name NOT IN ('ill', 'palciuser', 'libraryuse')
         AND ie.discovery_suppress = 'False'
 ORDER BY ie.effective_call_number;
 $$
