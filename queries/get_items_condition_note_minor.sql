85d3976502f5660782b31a83f31e01dc72360404
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-03-26 11:30:55 -0400

    Refactor get_items_condition_note_minor function
    Updated the function to retrieve items with an Inventory Note about Minor condition fixes, including changes to the return type and query structure.

--- queries/get_items_condition_note_minor.sql
+++ queries/get_items_condition_note_minor.sql
@@ -1,12 +1,13 @@
---metadb:function get_items_with_spine_label_note
--- This function retrieves all items with an Inventory Note about the Spine Label needing to be replaced.
-CREATE FUNCTION get_items_with_spine_label_note()
+--metadb:function get_items_condition_note_minor
+-- This function retrieves all items with an Inventory Note about Minor condition fix needed.
+DROP FUNCTION IF EXISTS get_items_condition_note_minor;
+CREATE FUNCTION get_items_condition_note_minor()
 RETURNS TABLE
 (
     item_barcode TEXT,
     item_call_number TEXT,
     item_title TEXT,
-    item_updated_date DATE
+    item_updated_date TEXT
 ) 
 AS
 $$
@@ -14,13 +15,26 @@ SELECT
     ihi.barcode AS item_barcode,
     ie.effective_call_number AS item_call_number,
     ihi.title AS item_title,
-    cast(ie.updated_date AS date) AS item_updated_date
-FROM
-    folio_derived.items_holdings_instances ihi
-    JOIN folio_derived.item_notes in2 ON in2.item_id = ihi.item_id
-    JOIN folio_derived.item_ext ie ON ie.item_id = ihi.item_id
-WHERE
-    in2.note_type_name = 'Inventoried Condition'
-    AND in2.note LIKE 'Minor%';
+    to_char(ie.updated_date::TIMESTAMPTZ, 'YYYY-MM-DD') AS item_updated_date
+FROM (
+    SELECT DISTINCT ON (item_id)
+        item_id, note, note_type_name
+    FROM folio_derived.item_notes
+    WHERE note_type_name = 'Inventoried Condition'
+    AND note LIKE 'Minor%'
+    ORDER BY item_id
+) in2
+    JOIN (
+        SELECT DISTINCT ON (item_id)
+            item_id, barcode, title
+        FROM folio_derived.items_holdings_instances
+        ORDER BY item_id
+    ) ihi ON ihi.item_id = in2.item_id
+    JOIN (
+        SELECT DISTINCT ON (item_id)
+            item_id, effective_call_number, updated_date
+        FROM folio_derived.item_ext
+        ORDER BY item_id
+    ) ie ON ie.item_id = in2.item_id
 $$
-LANGUAGE SQL;
+LANGUAGE SQL STABLE;
