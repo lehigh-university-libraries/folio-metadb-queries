4173897fbf8d1e523de40d80a793a8fde4ecf0b3
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-03-26 10:53:37 -0400

    Refactor get_inventory_counts function and query
    Updated the get_inventory_counts function to change item_count type from TEXT to BIGINT, and optimized the SQL query structure for better performance.

--- queries/get_inventory_counts.sql
+++ queries/get_inventory_counts.sql
@@ -1,46 +1,71 @@
 --metadb:function get_inventory_counts
 -- This function retrieves inventory counts for all library locations.
+DROP FUNCTION IF EXISTS get_inventory_counts;
 CREATE FUNCTION get_inventory_counts()
 RETURNS TABLE
 (
     library_name TEXT,
-    item_count TEXT
+    item_count BIGINT
 ) 
 AS
 $$
-
 SELECT
     *
 FROM (
     SELECT
         ll.library_name,
         COUNT(*) AS item_count
-    FROM
-        folio_derived.items_holdings_instances ihi
-        JOIN folio_derived.item_ext ie ON ie.item_id = ihi.item_id
-        JOIN folio_derived.item_statistical_codes isc ON isc.item_id = ihi.item_id
+    FROM (
+        SELECT DISTINCT ON (item_id)
+            item_id
+        FROM folio_derived.item_statistical_codes
+        WHERE statistical_code_name = 'Shelf Reading 2025'
+        ORDER BY item_id
+    ) isc
+        JOIN (
+            SELECT DISTINCT ON (item_id)
+                item_id
+            FROM folio_derived.items_holdings_instances
+            ORDER BY item_id
+        ) ihi ON ihi.item_id = isc.item_id
+        JOIN (
+            SELECT DISTINCT ON (item_id)
+                item_id, effective_location_id
+            FROM folio_derived.item_ext
+            ORDER BY item_id
+        ) ie ON ie.item_id = isc.item_id
         JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
-    WHERE
-        isc.statistical_code_name = 'Shelf Reading 2025'
     GROUP BY
         ll.library_name
     UNION ALL
     SELECT
         'TOTAL' AS library_name,
         COUNT(*) AS item_count
-    FROM
-        folio_derived.items_holdings_instances ihi
-        JOIN folio_derived.item_ext ie ON ie.item_id = ihi.item_id
-        JOIN folio_derived.item_statistical_codes isc ON isc.item_id = ihi.item_id
+    FROM (
+        SELECT DISTINCT ON (item_id)
+            item_id
+        FROM folio_derived.item_statistical_codes
+        WHERE statistical_code_name = 'Shelf Reading 2025'
+        ORDER BY item_id
+    ) isc
+        JOIN (
+            SELECT DISTINCT ON (item_id)
+                item_id
+            FROM folio_derived.items_holdings_instances
+            ORDER BY item_id
+        ) ihi ON ihi.item_id = isc.item_id
+        JOIN (
+            SELECT DISTINCT ON (item_id)
+                item_id, effective_location_id
+            FROM folio_derived.item_ext
+            ORDER BY item_id
+        ) ie ON ie.item_id = isc.item_id
         JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
-    WHERE
-        isc.statistical_code_name = 'Shelf Reading 2025') AS combined
+) AS combined
 ORDER BY
-    CASE WHEN library_name = 'TOTAL' THEN
-        1
-    ELSE
-        0
+    CASE WHEN library_name = 'TOTAL' THEN 1
+    ELSE 0
     END,
     item_count DESC;
 $$
-LANGUAGE SQL;
+LANGUAGE SQL STABLE;
