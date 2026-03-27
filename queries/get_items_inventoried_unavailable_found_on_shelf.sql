3c778c338ddd5f41fff537818022dbcaf3295712
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-03-27 15:50:41 -0400

    Refactor get_items_inventoried_unavailable_found_on_shelf function
    Updated the function to return item_updated_date as text and modified the SQL query for better performance.

--- queries/get_items_inventoried_unavailable_found_on_shelf.sql
+++ queries/get_items_inventoried_unavailable_found_on_shelf.sql
@@ -1,7 +1,6 @@
 --metadb:function get_items_inventoried_unavailable_found_on_shelf
---This fuction returns items inventoried with message 'unavailable item found on shelf'
-
-CREATE FUNCTION get_items_inventoried_unavailable_found_on_shelf ()
+DROP FUNCTION IF EXISTS get_items_inventoried_unavailable_found_on_shelf;
+CREATE FUNCTION get_items_inventoried_unavailable_found_on_shelf()
     RETURNS TABLE (
         item_status text,
         item_location text,
@@ -9,10 +8,10 @@ CREATE FUNCTION get_items_inventoried_unavailable_found_on_shelf ()
         call_number text,
         item_title text,
         item_note text,
-        item_updated_date date
+        item_updated_date text
     )
 AS $$
-    SELECT DISTINCT
+    SELECT
         iext.status_name AS item_status,
         iext.effective_location_name AS item_location,
         iext.barcode AS item_barcode,
@@ -24,12 +23,35 @@ AS $$
         )) AS call_number,
         inst.title AS item_title,
         inot.note AS item_note,
-        cast(iext.updated_date AS date) AS item_updated_date
-    FROM
-        folio_derived.item_ext iext
-    LEFT JOIN folio_derived.item_notes inot ON iext.item_id = inot.item_id
-    LEFT JOIN folio_derived.holdings_ext hrt ON iext.holdings_record_id = hrt.id
-    LEFT JOIN folio_derived.instance_ext inst ON hrt.instance_id = inst.instance_id
+        LEFT(iext.updated_date, 10) AS item_updated_date
+    FROM (
+        SELECT DISTINCT ON (item_id)
+            item_id, status_name, effective_location_name, barcode,
+            effective_call_number_prefix, effective_call_number,
+            volume, copy_number, updated_date, holdings_record_id,
+            effective_location_id
+        FROM folio_derived.item_ext
+        ORDER BY item_id
+    ) iext
+    LEFT JOIN (
+        SELECT DISTINCT ON (item_id)
+            item_id, note, note_type_name
+        FROM folio_derived.item_notes
+        WHERE note LIKE 'Shelf status: Unavailable item is on shelf%'
+        ORDER BY item_id
+    ) inot ON iext.item_id = inot.item_id
+    LEFT JOIN (
+        SELECT DISTINCT ON (id)
+            id, instance_id
+        FROM folio_derived.holdings_ext
+        ORDER BY id
+    ) hrt ON iext.holdings_record_id = hrt.id
+    LEFT JOIN (
+        SELECT DISTINCT ON (instance_id)
+            instance_id, title
+        FROM folio_derived.instance_ext
+        ORDER BY instance_id
+    ) inst ON hrt.instance_id = inst.instance_id
     LEFT JOIN folio_derived.locations_libraries locl ON iext.effective_location_id = locl.location_id
     WHERE
         inot.note LIKE 'Shelf status: Unavailable item is on shelf%'
@@ -38,5 +60,4 @@ AS $$
         item_location,
         call_number;
 $$
-LANGUAGE SQL;
-
+LANGUAGE SQL STABLE;
