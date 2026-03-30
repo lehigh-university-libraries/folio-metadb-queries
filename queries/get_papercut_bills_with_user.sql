59fcc6c0d35d6382a155a185ab8cda72303ac647
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-03-30 13:30:55 -0400

    Refactor get_papercut_bills_with_user function

--- queries/get_papercut_bills_with_user.sql
+++ queries/get_papercut_bills_with_user.sql
@@ -1,10 +1,11 @@
 --metadb:function get_papercut_bills_with_user
 -- This function retrieves all outstanding Papercut bills for a given user.
+--metadb:function get_papercut_bills_with_user
+DROP FUNCTION IF EXISTS get_papercut_bills_with_user;
 CREATE FUNCTION get_papercut_bills_with_user()
-
 RETURNS TABLE
 (
-    action_date DATE,
+    action_date TEXT,
     action_type TEXT,    
     bill_amount NUMERIC,
     account_balance NUMERIC,
@@ -13,18 +14,34 @@ RETURNS TABLE
 ) 
 AS
 $$
-select cast(faa.transaction_date as DATE) as action_date, 
-faa.type_action as action_type,
-faa.transaction_amount as bill_amount, 
-faa.account_balance as account_balance, 
-ug2.barcode AS user_barcode,  
-ug2.user_last_name AS last_name
-from folio_derived.feesfines_accounts_actions faa
-join folio_derived.users_groups ug2 on faa.user_id = ug2.user_id
-join folio_feesfines.accounts__t ffa on faa.account_id = ffa.id
-where faa.fine_status = 'Open'
-  AND faa.type_action = 'Papercut'
- order by faa.transaction_date desc;
-;
+SELECT
+    to_char(faa.transaction_date, 'YYYY-MM-DD') AS action_date,
+    faa.type_action AS action_type,
+    faa.transaction_amount AS bill_amount,
+    faa.account_balance AS account_balance,
+    ug2.barcode AS user_barcode,
+    ug2.user_last_name AS last_name
+FROM (
+    SELECT DISTINCT ON (account_id)
+        account_id, transaction_date, type_action, transaction_amount,
+        account_balance, user_id, fine_status
+    FROM folio_derived.feesfines_accounts_actions
+    WHERE fine_status = 'Open'
+        AND type_action = 'Papercut'
+    ORDER BY account_id, transaction_date DESC
+) faa
+JOIN (
+    SELECT DISTINCT ON (user_id)
+        user_id, barcode, user_last_name
+    FROM folio_derived.users_groups
+    ORDER BY user_id
+) ug2 ON faa.user_id = ug2.user_id
+JOIN (
+    SELECT DISTINCT ON (id)
+        id
+    FROM folio_feesfines.accounts__t
+    ORDER BY id
+) ffa ON faa.account_id = ffa.id
+ORDER BY faa.transaction_date DESC;
 $$
 LANGUAGE SQL STABLE;
