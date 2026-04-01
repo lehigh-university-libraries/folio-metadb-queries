4083f1dd7926be8ca7f417ea50d1a3db2128f712
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-04-01 15:13:40 -0400

    Modify SQL query to count 'In transit' items
    include total count

--- queries/get_missing_by_library.sql
+++ queries/get_missing_by_library.sql
@@ -9,25 +9,51 @@ RETURNS TABLE
 ) 
 AS
 $$
-SELECT
-    ll.library_name,
-    COUNT(*) AS item_count
+SELECT *
 FROM (
-    SELECT DISTINCT ON (item_id)
-        item_id
-    FROM folio_derived.items_holdings_instances
-    ORDER BY item_id
-) ihi
-JOIN (
-    SELECT DISTINCT ON (item_id)
-        item_id, effective_location_id, status_name, discovery_suppress
-    FROM folio_derived.item_ext
-    WHERE status_name = 'Missing'
-        AND discovery_suppress = 'False'
-    ORDER BY item_id
-) ie ON ie.item_id = ihi.item_id
-JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
-GROUP BY ll.library_name
-ORDER BY ll.library_name;
+    SELECT
+        ll.library_name,
+        COUNT(*) AS item_count
+    FROM (
+        SELECT DISTINCT ON (item_id)
+            item_id
+        FROM folio_derived.items_holdings_instances
+        ORDER BY item_id
+    ) ihi
+    JOIN (
+        SELECT DISTINCT ON (item_id)
+            item_id, effective_location_id, status_name, discovery_suppress
+        FROM folio_derived.item_ext
+        WHERE status_name = 'In transit'
+            AND discovery_suppress = 'False'
+        ORDER BY item_id
+    ) ie ON ie.item_id = ihi.item_id
+    JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
+    GROUP BY ll.library_name
+
+    UNION ALL
+
+    SELECT
+        'TOTAL' AS library_name,
+        COUNT(*) AS item_count
+    FROM (
+        SELECT DISTINCT ON (item_id)
+            item_id
+        FROM folio_derived.items_holdings_instances
+        ORDER BY item_id
+    ) ihi
+    JOIN (
+        SELECT DISTINCT ON (item_id)
+            item_id, effective_location_id, status_name, discovery_suppress
+        FROM folio_derived.item_ext
+        WHERE status_name = 'In transit'
+            AND discovery_suppress = 'False'
+        ORDER BY item_id
+    ) ie ON ie.item_id = ihi.item_id
+    JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
+) AS combined
+ORDER BY
+    CASE WHEN library_name = 'TOTAL' THEN 1 ELSE 0 END,
+    item_count DESC;
 $$
 LANGUAGE SQL STABLE;
