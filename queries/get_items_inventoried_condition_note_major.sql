e1d7361a787f487c57c2678461ffbe00c99b44a7
Mark Canney <113544083+mac609@users.noreply.github.com>
2026-04-02 10:12:10 -0400

    Refactor SQL query for item inventory retrieval
    Refactored SQL query to include library name and updated date while removing unnecessary joins and conditions.

--- queries/get_items_inventoried_condition_note_major.sql
+++ queries/get_items_inventoried_condition_note_major.sql
@@ -12,39 +12,34 @@ RETURNS TABLE
 AS
 $$
 SELECT
+    ll.library_name,
     ihi.barcode AS item_barcode,
     ie.effective_call_number AS item_call_number,
     ihi.title AS item_title,
-    in2.note AS missing_searched_note  
+    LEFT(ie.updated_date, 10) AS item_updated_date
 FROM (
     SELECT DISTINCT ON (item_id)
-        item_id
-    FROM folio_derived.item_statistical_codes
-    WHERE statistical_code_name = 'Shelf Reading 2025'
+        item_id, note, note_type_name
+    FROM folio_derived.item_notes
+    WHERE note_type_name = 'Inventoried Condition'
+    AND note LIKE 'Major%'
     ORDER BY item_id
-) isc
+) in2
     JOIN (
         SELECT DISTINCT ON (item_id)
             item_id, barcode, title
         FROM folio_derived.items_holdings_instances
         ORDER BY item_id
-    ) ihi ON ihi.item_id = isc.item_id
+    ) ihi ON ihi.item_id = in2.item_id
     JOIN (
         SELECT DISTINCT ON (item_id)
-            item_id, effective_call_number, status_name, effective_location_name
+            item_id, effective_call_number, updated_date, effective_location_id
         FROM folio_derived.item_ext
         ORDER BY item_id
-    ) ie ON ie.item_id = isc.item_id
-    LEFT JOIN (
-        SELECT DISTINCT ON (item_id)
-            item_id, note, note_type_name
-        FROM folio_derived.item_notes
-        WHERE note_type_name = 'Workflow-Missing Searched'
-        ORDER BY item_id
-    ) in2 ON in2.item_id = isc.item_id
+    ) ie ON ie.item_id = in2.item_id
+    JOIN folio_derived.locations_libraries ll ON ll.location_id = ie.effective_location_id
 WHERE
-    ie.status_name = 'Missing'
-    AND ie.effective_location_name LIKE 'Fairchild%'
-ORDER BY ie.effective_call_number;
+    (location_filter = 'All Locations' OR ll.library_name = location_filter)
+ORDER BY ll.library_name, ie.effective_call_number;
 $$
 LANGUAGE SQL STABLE;
